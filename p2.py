e38ac64bc525b9bd7fc725760d84366f838f0097
RenkoT97 <tjasa.renko@gmail.com>
2019-10-20 02:13:01 +0200

    Add files via upload

--- p2.py
+++ p2.py
@@ -2,18 +2,12 @@ import networkx as nx
 import numpy as np
 import matplotlib as mpl
 
-n = 5
+n = 10
 
 drevesa_reda_n = nx.nonisomorphic_trees(n, create='graph')
 sez = []
 for drevo in drevesa_reda_n:
     sez.append(drevo)
-
-'''
-Ne vem, zakaj list(graf.neighbors(i)) ne dela. V drugem filu dela. Sicer
-bi moral že graf.neighbors(i) vrniti list, a ga ne. Mislim, da bo ostalo
-pravilno delovalo (ampak zelo počasi).
-'''
 
 def seznam_sosedov(graf):
     return[[i, list(graf.neighbors(i))] for i in graf]
@@ -24,16 +18,19 @@ def listi(drevo):
 novi = []
 for T in sez:
     w = nx.wiener_index(T, weight=None)
-    for list in listi(T):
+    for lis in listi(T):
         for vozlisce in T:
             G = T.copy()
-            G.remove_node(list)
-            G.add_node(list)
-            G.add_edge(list,vozlisce)
+            G.remove_node(lis)
+            G.add_node(lis)
+            G.add_edge(lis,vozlisce)
             v = nx.wiener_index(G, weight=None)
             if abs(w-v)==1:
                 novi.append(G)
 
-print(novi)
-
-D = np.argmax(nx.diameter())
+if novi:
+    di_novi = [nx.diameter(el) for el in novi]
+    m = np.argmax(di_novi)
+    D = novi[m]
+else:
+    print("D ne obstaja")
