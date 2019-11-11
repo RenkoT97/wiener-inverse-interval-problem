c066f548ec2517b8904c57643090a4620b7aab2d
Tjaša Renko <tjasa.renko@gmail.com>
2019-11-12 00:17:20 +0100

    Popravljeno iskanje maksimumov p1, da ni težav s prostorsko zahtevnostjo

--- maksimumi_p1.py
+++ maksimumi_p1.py
@@ -7,21 +7,7 @@ import math
 
 start_time = time.time()
 
-n = 300
-
-def nakljucno_drevo(n):
-    sez = [i for i in range(n)]
-    rd.shuffle(sez)
-    g = nx.Graph()
-    g.add_nodes_from(sez)
-    sez2 = []
-    sez2.append(sez.pop())
-    while sez:
-        a = sez.pop()
-        b = rd.choice(sez2)
-        sez2.append(a)
-        g.add_edge(a, b)
-    return g
+n = 100
 
 def prilagojeno_drevo(n):
     graf = nx.Graph()
@@ -38,69 +24,21 @@ def prilagojeno_drevo(n):
         graf.add_edge(a,b)
     return graf       
 
-def naredi_drevesa(st_vozlisc, st_grafov):
-    sez = []
-    for i in range(st_grafov):
-        sez.append(nakljucno_drevo(st_vozlisc))
-    return sez
+graf = prilagojeno_drevo(n)
 
-#drevesa = naredi_drevesa(n,10)
-drevesa = [prilagojeno_drevo(n) for i in range(10)]
+def moc_mnozice_novih_indeksov(graf):
+    n = len(graf)
+    osnovni_index = nx.wiener_index(graf)
+    najkrajse_poti = nx.all_pairs_shortest_path(graf)
+    I = set()
+    for i in range(n):
+        vozlisce, slovar_poti = next(najkrajse_poti)
+        vsota_poti_iz_vozlisca = sum(len(slovar_poti[kljuc]) for kljuc in slovar_poti)
+        nov_index = osnovni_index + vsota_poti_iz_vozlisca + n
+        I.add(int(nov_index))
+    return len(I)
 
-def najkrajse_poti(graf):
-    return list(nx.all_pairs_shortest_path(graf))
-#seznam vsot poti iz posameznega vozlišča
-
-def seznam_vsot_poti(drevesa):
-    s = []
-    for sez_poti in [najkrajse_poti(drevo) for drevo in drevesa]:
-        sez = []
-        for vozlisce_s_potmi in sez_poti:
-            vsota = sum(len(vozlisce_s_potmi[1][kljuc]) - 1 for kljuc in vozlisce_s_potmi[1])
-            sez.append(vsota)
-        s.append(sez)
-    return s  
-
-vsote_poti = seznam_vsot_poti(drevesa)
-
-def wienerjev_index_s_potmi(seznam_vsot_poti):
-    return [sum(vsote)/2 for vsote in seznam_vsot_poti]
-
-wienerjev_index = wienerjev_index_s_potmi(vsote_poti)
-
-#M je množica vseh dreves z n+1 vozlišči, ki jih dobimo, če drevesu T dodamo list
-
-def mnozica_dreves_z_dodanim_listom(drevesa, vsote_poti, wienerjev_index):
-    #če želimo videti nova drevesa, odkomentiramo dele z M: zakomentirano, ker nepotrebno in tako dela hitreje
-    sez = []
-    ind = []
-    i = 0
-    for drevo in drevesa:
-        #M = []
-        I = set()
-        n = nx.number_of_nodes(drevo)
-        osnovni_index = wienerjev_index[i]
-        i += 1
-        j = 0
-        for vozlisce in drevo:
-            #T = drevo.copy()
-            #T.add_node(n+1)
-            #T.add_edge(vozlisce,n+1)
-            #M.append(T)
-            poti_iz_vozlisca = vsote_poti[i-1][j]
-            index = osnovni_index + poti_iz_vozlisca + n
-            I.add(index)
-            j += 1
-        #sez.append(M)
-        ind.append(I)
-    return sez, ind
-nova_drevesa, indeksi_novih_dreves = mnozica_dreves_z_dodanim_listom(drevesa, vsote_poti, wienerjev_index)
-
-def moc_mnozic_indeksov_naddreves_dreves(indeksi_poddreves):
-    return [len(indeksi) for indeksi in indeksi_poddreves]
-
-moc_mnozic_indeksov_za_drevesa = moc_mnozic_indeksov_naddreves_dreves(indeksi_novih_dreves)
-print(moc_mnozic_indeksov_za_drevesa)
+moc_mnozice_novih_indeksov(graf)
 
 def narisi(drevo):
     slika = nx.draw(drevo,node_size=4)
@@ -111,7 +49,7 @@ def seznam_sosedov(graf):
     return[[i, list(graf.neighbors(i))] for i in graf]
 
 def listi(drevo):
-    return [l[0] for l in seznam_sosedov(drevo) if len(l[1])==1]
+    return [l[0] for l in seznam_sosedov(drevo) if len(l[1]) == 1]
 
 def vozlisca(drevo):
     return [i for i in drevo]
@@ -123,12 +61,8 @@ def sosed(drevo):
     novo_drevo.remove_node(l)
     novo_drevo.add_node(l)
     novo_drevo.add_edge(l, vozlisce)
-    novo_drevo = [novo_drevo]
-    vs_poti = seznam_vsot_poti(novo_drevo)
-    w = wienerjev_index_s_potmi(vs_poti)
-    poddrevesa, indeksi_poddreves = mnozica_dreves_z_dodanim_listom(novo_drevo, vs_poti,w)
-    moc_mnozice = moc_mnozic_indeksov_naddreves_dreves(indeksi_poddreves)
-    return novo_drevo[0], moc_mnozice[0]
+    I = moc_mnozice_novih_indeksov(novo_drevo)
+    return novo_drevo, I
 
 def P(e,en,t):
     #po Kirkpatricku
@@ -137,10 +71,10 @@ def P(e,en,t):
     else:
         return math.e ** ((e - en) / t)
     
-def simulated_annealing(mesto_drevesa_v_seznamu_dreves, kmax, emax, zacetna_temperatura = 100):
+def simulated_annealing(drevo, kmax, emax, zacetna_temperatura = 100):
     #kmax največje število korakov, emax zadovoljiv rezultat
-    stanje = drevesa[mesto_drevesa_v_seznamu_dreves]
-    energija = moc_mnozic_indeksov_za_drevesa[mesto_drevesa_v_seznamu_dreves]
+    stanje = drevo
+    energija = moc_mnozice_novih_indeksov(drevo)
     najboljse_stanje = stanje
     najboljsa_energija = energija
     k = 0
@@ -156,20 +90,23 @@ def simulated_annealing(mesto_drevesa_v_seznamu_dreves, kmax, emax, zacetna_temp
         k += 1   
     return najboljse_stanje, najboljsa_energija
 
-def ozji_izbor_dreves(drevesa):
-    k = 4 * len(drevesa) // 5
+def ozji_izbor_dreves(st_vozlisc, st_dreves = 10):
+    sez_dreves = [prilagojeno_drevo(n) for i in range(st_dreves)]
+    moc_mnozic_indeksov_za_drevesa = [moc_mnozice_novih_indeksov(drevo) for drevo in sez_dreves]
+    k = 4 * st_dreves // 5
     while k > 0:
         k -= 1
         i = np.argmin(moc_mnozic_indeksov_za_drevesa)
-        del drevesa[i]
+        del sez_dreves[i]
         del moc_mnozic_indeksov_za_drevesa[i]
-    return drevesa, moc_mnozic_indeksov_za_drevesa
+    return sez_dreves, moc_mnozic_indeksov_za_drevesa
 
-drevesa, moc_mnozic_indeksov_za_drevesa = ozji_izbor_dreves(drevesa)
+drevesa, moc_mnozic_indeksov_za_drevesa = ozji_izbor_dreves(n, 10)
 print(drevesa)#test
+print(moc_mnozic_indeksov_za_drevesa)
 
 def maximum(drevesa, kmax = 10, emax = n, zacetna_temperatura = 100):
-    sez_maximumov = [simulated_annealing(i, kmax, emax, zacetna_temperatura) for i in range(len(drevesa))]
+    sez_maximumov = [simulated_annealing(i, kmax, emax, zacetna_temperatura) for i in drevesa]
     print(sez_maximumov) #test
     sez_moci = [el[1] for el in sez_maximumov]
     print(sez_moci) #test
@@ -177,6 +114,7 @@ def maximum(drevesa, kmax = 10, emax = n, zacetna_temperatura = 100):
 
 max_drevo, max_moc_mnozice = maximum(drevesa)
 print(max_moc_mnozice)
-narisi(max_drevo)
 
 print("%s seconds" % (time.time() - start_time))
+
+narisi(max_drevo)
